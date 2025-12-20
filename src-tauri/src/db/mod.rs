43733e435ddc038ce21fada9b2df8af484b68e33
erlangzhenjun <1173957018@qq.com>
2025-12-20 22:38:28 +0800

    test: 新增清理相关测试

--- src-tauri/src/db/mod.rs
+++ src-tauri/src/db/mod.rs
@@ -124,6 +124,9 @@ mod test_db_private;
 #[cfg(test)]
 #[path = "../test_unit/test_db_sync.rs"]
 mod test_db_sync;
+#[cfg(test)]
+#[path = "../test_unit/test_db_cleanup.rs"]
+mod test_db_cleanup;
 
 #[cfg(test)]
 pub static TEST_RUN_LOCK: std::sync::Mutex<()> = std::sync::Mutex::new(());
